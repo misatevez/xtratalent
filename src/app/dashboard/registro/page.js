16f17aba879db87b4958db97ac3061a48e0bad98
Misael <misatevez@gmail.com>
2023-12-20 11:41:02 -0300

    selectora personal

--- src/app/dashboard/registro/page.js
+++ src/app/dashboard/registro/page.js
@@ -1,8 +1,10 @@
 import { RegistroInterno } from "@/components/registro-interno";
+import { SelectoraPersonal } from "@/components/selectora-personal";
 
 export default function page() {
     return (
- <RegistroInterno />
+ 
+        <SelectoraPersonal />
     );
 }
 
