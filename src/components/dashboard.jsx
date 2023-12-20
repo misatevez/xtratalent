1e2e8423dca30f467e44f497a8d45607b86d240f
Misael <misatevez@gmail.com>
2023-12-19 22:09:58 -0300

    init

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,271 @@
+
+import Link from "next/link"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar"
+import { CardTitle, CardDescription, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Toggle } from "@/components/ui/toggle"
+import { RegisterUsuariosInternos } from "./registerInterno"
+import { NuevosGruposCorporativos } from "./nuevos-grupos-corporativos"
+import Login from "./login"
+
+export function Dashboard() {
+  return (
+    (<div key="1" className="flex flex-col w-full min-h-screen">
+      <aside
+        className="w-64 h-full bg-gray-100 dark:bg-gray-800 border-r fixed top-0 left-0 overflow-auto">
+        <div className="flex items-center justify-center h-16 border-b">
+          {/* <img alt="SaaS App Logo" className="h-8 w-auto" src="/placeholder.svg" /> */}
+          <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-100 ml-4">Xtra Talent</h1>
+        </div>
+        <nav className="flex flex-col p-4">
+          <Link
+            className="flex items-center gap-2 py-2 text-lg font-semibold text-gray-800 dark:text-gray-100"
+            href="#">
+            <LockIcon className="w-6 h-6" />
+            Seguridad y acceso
+          </Link>
+          <Link
+            className="flex items-center gap-2 py-2 text-lg font-semibold text-gray-800 dark:text-gray-100"
+            href="#">
+           <StarIcon className="w-6 h-6" />
+            Administracion de personal
+          </Link>
+          <Link
+            className="flex items-center gap-2 py-2 text-lg font-semibold text-gray-800 dark:text-gray-100"
+            href="#">
+            <HelpCircleIcon className="w-6 h-6" />
+            Evaluaciones
+          </Link>
+          <Link
+            className="flex items-center gap-2 py-2 text-lg font-semibold text-gray-800 dark:text-gray-100"
+            href="#">
+            <ImageIcon className="w-6 h-6" />
+            Tipos de contenido
+          </Link>
+          <Link
+            className="flex items-center gap-2 py-2 text-lg font-semibold text-gray-800 dark:text-gray-100"
+            href="#">
+            <CopyrightIcon className="w-6 h-6" />
+            Licencia y soporte
+          </Link>
+          <Link
+            className="flex items-center gap-2 py-2 text-lg font-semibold text-gray-800 dark:text-gray-100"
+            href="#">
+             <SettingsIcon className="w-6 h-6" />
+            Configuracion
+          </Link>
+        </nav>
+      </aside>
+      <div className="flex flex-col  min-h-screen pl-64">
+        <main>
+          
+        </main>
+      </div>
+    </div>)
+  );
+}
+
+
+function LayoutDashboardIcon(props) {
+  return (
+    (<svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <rect width="7" height="9" x="3" y="3" rx="1" />
+      <rect width="7" height="5" x="14" y="3" rx="1" />
+      <rect width="7" height="9" x="14" y="12" rx="1" />
+      <rect width="7" height="5" x="3" y="16" rx="1" />
+    </svg>)
+  );
+}
+function StarIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+    </svg>
+  )
+}
+
+function LockIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
+      <path d="M7 11V7a5 5 0 0 1 10 0v4" />
+    </svg>
+  )
+}
+
+function SettingsIcon(props) {
+  return (
+    (<svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <path
+        d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
+      <circle cx="12" cy="12" r="3" />
+    </svg>)
+  );
+}
+
+function CopyrightIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M15 9.354a4 4 0 1 0 0 5.292" />
+    </svg>
+  )
+}
+
+
+function HelpCircleIcon(props) {
+  return (
+    (<svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <circle cx="12" cy="12" r="10" />
+      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+      <path d="M12 17h.01" />
+    </svg>)
+  );
+}
+
+
+function UserIcon(props) {
+  return (
+    (<svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+      <circle cx="12" cy="7" r="4" />
+    </svg>)
+  );
+}
+
+function ImageIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
+      <circle cx="9" cy="9" r="2" />
+      <path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21" />
+    </svg>
+  )
+}
+
+function CalendarIcon(props) {
+  return (
+    (<svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <rect width="18" height="18" x="3" y="4" rx="2" ry="2" />
+      <line x1="16" x2="16" y1="2" y2="6" />
+      <line x1="8" x2="8" y1="2" y2="6" />
+      <line x1="3" x2="21" y1="10" y2="10" />
+    </svg>)
+  );
+}
+
+
+function SearchIcon(props) {
+  return (
+    (<svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <circle cx="11" cy="11" r="8" />
+      <path d="m21 21-4.3-4.3" />
+    </svg>)
+  );
+}
---
+++ src/components/login.jsx
@@ -0,0 +1,174 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/k8dyNyHXaze
+ */
+import { Button } from "@/components/ui/button"
+
+export default function Login() {
+  return (
+    <>
+      <div className="min-h-screen bg-white flex">
+        <div className="flex-1 flex items-center justify-center p-12">
+          <div className="max-w-md w-full">
+            <div>
+              {/* <img
+                alt="Logo"
+                className="mx-auto h-12 w-auto"
+                height="48"
+                src="/placeholder.svg"
+                style={{
+                  aspectRatio: "48/48",
+                  objectFit: "cover",
+                }}
+                width="48"
+              /> */}
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">¡Bienvenidos!</h2>
+            </div>
+            <div className="mt-3">
+              <div>
+                <div>
+                  <p className="text-sm text-center font-medium text-gray-700">Ingresa a tu cuenta</p>
+                </div>
+              </div>
+              <div className="mt-6">
+                <div className="space-y-6">
+                  <div className="rounded-md shadow-sm -space-y-px">
+                    <div>
+                      <label className="sr-only" htmlFor="email-address">
+                        Email
+                      </label>
+                      <input
+                        autoComplete="email"
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        id="email-address"
+                        name="email"
+                        placeholder="Email"
+                        required
+                        type="email"
+                      />
+                    </div>
+                    <div>
+                      <label className="sr-only" htmlFor="password">
+                        Contraseña
+                      </label>
+                      <input
+                        autoComplete="current-password"
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        id="password"
+                        name="password"
+                        placeholder="Contraseña"
+                        required
+                        type="password"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <input
+                        className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                        id="remember-me"
+                        name="remember-me"
+                        type="checkbox"
+                      />
+                      <label className="ml-2 block text-sm text-gray-900" htmlFor="remember-me">
+                        Recordarme
+                      </label>
+                    </div>
+                    <div className="text-sm">
+                      <a className="font-medium text-indigo-600 hover:text-indigo-500" href="#">
+                        ¿Recuperar contraseña?
+                      </a>
+                    </div>
+                  </div>
+                  <div>
+                    <Button className="w-full" variant="default">
+                      Iniciar sesión
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-6">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center text-sm">
+                  <span className="px-2 bg-white text-gray-500">¿Necesitas ayuda?</span>
+                </div>
+              </div>
+              <div className="mt-6 grid grid-cols-2 gap-3">
+                <div>
+                  <Button className="w-full" variant="outline">
+                    <SmartphoneIcon className="text-green-500" />
+                    WhatsApp
+                  </Button>
+                </div>
+                <div>
+                  <Button className="w-full" variant="outline">
+                    <MailIcon className="text-blue-500" />
+                    Email
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden lg:block relative w-0 flex-1">
+        <img
+          alt="Background"
+          className="absolute inset-0 h-full w-full object-cover"
+          height="490"
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "861/490",
+            objectFit: "cover",
+          }}
+          width="861"
+        />
+      </div>
+    </>
+  )
+}
+
+function MailIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="20" height="16" x="2" y="4" rx="2" />
+      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+    </svg>
+  )
+}
+
+
+function SmartphoneIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="14" height="20" x="5" y="2" rx="2" ry="2" />
+      <path d="M12 18h.01" />
+    </svg>
+  )
+}
