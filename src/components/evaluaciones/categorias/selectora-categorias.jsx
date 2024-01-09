9dcee99d0800f3792368642f13142ce5e024f550
Misael <misatevez@gmail.com>
2024-01-08 22:31:49 -0300

    cambios en selectoras

--- src/components/evaluaciones/categorias/selectora-categorias.jsx
+++ src/components/evaluaciones/categorias/selectora-categorias.jsx
@@ -10,7 +10,7 @@ export function SelectoraCategorias() {
     (
         <main
           className="p-8 space-y-8 mt-8 mb-8 mx-auto max-w-7xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-lg shadow-lg text-white">
-          <h1 className="text-4xl font-bold mb-6 text-center">Categorias</h1>
+          <h1 className="text-4xl font-bold mb-6 text-center">Familias</h1>
           <div className="flex items-center justify-center space-x-4">
             <Button  onClick={() => router.push('/dashboard/evaluaciones/categorias/crearcategoria')} className="bg-white text-purple-500 hover:bg-gray-200">Crear familia</Button>
             <Button  onClick={() => router.push('/dashboard/evaluaciones/categorias/crearcategoria/reportes')} className="bg-white text-purple-500 hover:bg-gray-200">Catálogo familias</Button> 
--- src/components/evaluaciones/categorias/subcategorias/selectora-subcategorias.jsx
+++ src/components/evaluaciones/categorias/subcategorias/selectora-subcategorias.jsx
@@ -10,7 +10,7 @@ export function SelectoraSubCategorias() {
     (
         <main
           className="p-8 space-y-8 mt-8 mb-8 mx-auto max-w-7xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-lg shadow-lg text-white">
-          <h1 className="text-4xl font-bold mb-6 text-center">Categorias</h1>
+          <h1 className="text-4xl font-bold mb-6 text-center">Sub-familias</h1>
           <div className="flex items-center justify-center space-x-4">
             <Button  onClick={() => router.push('/dashboard/evaluaciones/categorias/subcategorias/crearsubcategorias')} className="bg-white text-purple-500 hover:bg-gray-200">Crear sub-familia</Button>
             <Button   className="bg-white text-purple-500 hover:bg-gray-200">Catálogo sub-familias</Button> 
